ccdd389d6d6bd1ac71a3f9642fc38ea9b45be634
Jason Yundt <jason@jasonyundt.email>
2023-02-01 07:44:20 -0500

    Add file that’s fully compliant with the REUSE Spec

---
+++ fully_compliant.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023 Jason Yundt <jason@jasonyundt.email>
+# SPDX-License-Identifier: Apache-2.0
+
+print("This file can contain comments, so it MUST contain a comment header.")
